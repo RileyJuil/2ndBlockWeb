018188b6c1ef59fcefa9bd24f112486d4f17b1f2
RileyJuil <riley@dunamu.com>
2022-04-01 11:15:45 +0900

    selenium Grid 적용

--- src/main/java/SetUp.java
+++ src/main/java/SetUp.java
@@ -3,9 +3,11 @@ package main.java;
 import java.io.IOException;
 import java.net.URL;
 
+import org.openqa.selenium.Platform;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
@@ -20,19 +22,24 @@ public class SetUp {
 
 		System.setProperty("webdriver.chrome.driver", "C:\\Users\\Phillip\\Downloads\\chromedriver.exe");
 		
-		ChromeOptions chromeOptions = new ChromeOptions();
-		WebDriver driver = new RemoteWebDriver(new URL("http://localhost:4444/wd/hub"), chromeOptions);
+		DesiredCapabilities cap = new DesiredCapabilities();
+		cap.setBrowserName("chrome");
+		cap.setPlatform(Platform.WINDOWS);
+		URL url = new URL("http://localhost:4444/wd/hub");
+		WebDriver driver = new RemoteWebDriver(url, cap);
+		driver.manage().window().maximize();
 		driver.get("https://2ndblock.com/");
 		
 		
 		
-				
 		
-		/*
-		 * WebDriver driver = new ChromeDriver(); driver.manage().window().maximize();
-		 * // driver.get("https://www.secondblock-front.dunamu-2ndblock-dev.com/");
-		 * //DEV // 서버 driver.get("https://2ndblock.com/"); // PROD 서버
-		 */
+//		ChromeOptions chromeOptions = new ChromeOptions();
+//		WebDriver driver = new RemoteWebDriver(new URL("http://localhost:4444/wd/hub"), chromeOptions);
+//		
+//		WebDriver driver = new ChromeDriver(); driver.manage().window().maximize();
+//		driver.get("https://www.secondblock-front.dunamu-2ndblock-dev.com/");
+//		driver.get("https://2ndblock.com/"); // PROD 서버
+
 		Common = new Common(driver);
 	}
 
