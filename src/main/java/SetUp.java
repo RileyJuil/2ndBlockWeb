5483b5041e8343880dece6ded581cb81aa34c7eb
RileyJuil <riley@dunamu.com>
2022-04-01 14:15:08 +0900

    romote jenkins

--- src/main/java/SetUp.java
+++ src/main/java/SetUp.java
@@ -3,7 +3,6 @@ package main.java;
 import java.io.IOException;
 import java.net.URL;
 
-import org.openqa.selenium.Platform;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
