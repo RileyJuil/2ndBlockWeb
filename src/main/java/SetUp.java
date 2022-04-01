6cfbbda7c2b2ba8f837e01b88b73b25e93cdc1a1
RileyJuil <riley@dunamu.com>
2022-04-01 16:01:22 +0900

    로걸에서 실행되는 부분에서부터 다시 시작

--- src/main/java/SetUp.java
+++ src/main/java/SetUp.java
@@ -3,14 +3,20 @@ package main.java;
 import java.io.IOException;
 import java.net.URL;
 
+import org.junit.Before;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.remote.Browser;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Optional;
+import org.testng.annotations.Parameters;
+
+import io.appium.java_client.remote.MobileCapabilityType;
 
 
 public class SetUp {
@@ -19,23 +25,18 @@ public class SetUp {
 	public static Common Common = null;
 	
 	@BeforeClass
-	public static void main(String[] args) throws IOException {
+	public void createAndStartService() throws IOException {
 
 		System.setProperty("webdriver.chrome.driver", "C:\\Users\\Phillip\\Downloads\\chromedriver.exe");
 		
-		ChromeOptions chromeOptions = new ChromeOptions();
-		WebDriver driver = new RemoteWebDriver(new URL("http://localhost:4444/wd/hub"), chromeOptions);
+		driver = new ChromeDriver();
 		driver.manage().window().maximize();
-		driver.get("https://2ndblock.com/");
-		
-			
-		
+		driver.get("https://2ndblock.com/"); // PROD 서버
+
 //		ChromeOptions chromeOptions = new ChromeOptions();
 //		WebDriver driver = new RemoteWebDriver(new URL("http://localhost:4444/wd/hub"), chromeOptions);
-//		
-//		WebDriver driver = new ChromeDriver(); driver.manage().window().maximize();
-//		driver.get("https://www.secondblock-front.dunamu-2ndblock-dev.com/");
-//		driver.get("https://2ndblock.com/"); // PROD 서버
+//		driver.manage().window().maximize();
+//		driver.get("https://2ndblock.com/");
 
 		Common = new Common(driver);
 	}
