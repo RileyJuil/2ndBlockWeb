3ed2a657611823ff26c8ac41eefd64ed6ed7bc0e
RileyJuil <riley@dunamu.com>
2022-04-01 10:52:25 +0900

    Chrome Remote Driver 적용

--- src/main/java/SetUp.java
+++ src/main/java/SetUp.java
@@ -1,9 +1,12 @@
 package main.java;
 
 import java.io.IOException;
+import java.net.URL;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 
@@ -16,12 +19,20 @@ public class SetUp {
 	public static void createAndStartService() throws IOException {
 
 		System.setProperty("webdriver.chrome.driver", "C:\\Users\\Phillip\\Downloads\\chromedriver.exe");
-		driver = new ChromeDriver();
-		driver.manage().window().maximize();
-		// driver.get("https://www.secondblock-front.dunamu-2ndblock-dev.com/"); //DEV
-		// 서버
-		driver.get("https://2ndblock.com/"); // PROD 서버
-
+		
+		ChromeOptions chromeOptions = new ChromeOptions();
+		WebDriver driver = new RemoteWebDriver(new URL("http://localhost:4444/wd/hub"), chromeOptions);
+		driver.get("https://2ndblock.com/");
+		
+		
+		
+				
+		
+		/*
+		 * WebDriver driver = new ChromeDriver(); driver.manage().window().maximize();
+		 * // driver.get("https://www.secondblock-front.dunamu-2ndblock-dev.com/");
+		 * //DEV // 서버 driver.get("https://2ndblock.com/"); // PROD 서버
+		 */
 		Common = new Common(driver);
 	}
 
