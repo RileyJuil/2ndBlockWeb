44f50c9c9d718d99c91c0773946534fca6658cb6
RileyJuil <riley@dunamu.com>
2022-04-05 13:38:31 +0900

    다시 Test.java 안으로

--- src/test/java/DeleteAccount.java
+++ src/test/java/DeleteAccount.java
@@ -24,7 +24,37 @@ import org.testng.annotations.Test;
 import main.java.Common;
 import main.java.SetUp;
 
-public class DeleteAccount extends SetUp{
+public class DeleteAccount {
+	
+	public static RemoteWebDriver driver;
+	public static Common Common = null;
+	
+	@BeforeTest
+	public static void createAndStartService() throws IOException {
+
+		//File file = new File("lib" + File.separator + "chromedriver.exe");
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\Phillip\\Downloads\\chromedriver.exe");
+			
+//		ChromeOptions chromeOptions = new ChromeOptions();
+//		RemoteWebDriver driver = new RemoteWebDriver(new URL("http://172.17.160.181:4444/wd/hub"), chromeOptions);
+//		driver.manage().window().maximize();
+//		driver.get("https://2ndblock.com/");
+//		Thread.sleep(5000);
+			
+//		DesiredCapabilities capabilities = new DesiredCapabilities();
+//		driver = new RemoteWebDriver(new URL("http://172.17.160.181:4444/wd/hub"), capabilities);
+//		driver.manage().window().maximize();
+//		driver.get("https://2ndblock.com/");
+
+	
+		driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("https://2ndblock.com/");
+
+		Common = new Common(driver);
+	}
+	
+	
 	
 		@Test
 		public void scenario_01_facebookLogin() throws InterruptedException {
