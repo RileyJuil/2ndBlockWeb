241e4c5bf566677c899031d292352ae2767eec5b
riley <riley@DESKTOP-IIOPE3V>
2022-06-21 11:47:00 +0900

    캐릭터 꾸미기 적용

--- src/test/java/Func_04_InGame.java
+++ src/test/java/Func_04_InGame.java
@@ -15,7 +15,7 @@ public class Func_04_InGame extends SetUp{
 	
 	
 		@Test
-		public void Home_001_InGame_HomeBtn() throws InterruptedException {
+		public void InGame_001_HomeBtn() throws InterruptedException {
 			
 			//Common.facebookLogin(); //페이스북 로그인
 	
@@ -27,7 +27,7 @@ public class Func_04_InGame extends SetUp{
 	  }
 	
 		@Test
-		public void Home_002_InGame_MyMenu_Character() throws InterruptedException {
+		public void InGame_002_MyMenu_Character() throws InterruptedException {
 			
 			Common.clickByXpath("/html/body/div[1]/div[1]/div[2]/div[1]/div[1]/div[2]/div/img"); //마이메뉴
 			Common.clickByText("캐릭터 꾸미기"); //"캐릭터 꾸미기" 클릭
@@ -36,7 +36,7 @@ public class Func_04_InGame extends SetUp{
 	  }
 
 		@Test
-		public void Home_003_InGame_MyMenu_Setting() throws InterruptedException {
+		public void InGame_003_MyMenu_Setting() throws InterruptedException {
 			
 			Common.clickByXpath("/html/body/div[1]/div[1]/div[2]/div[1]/div[1]/div[2]/div/img"); //마이메뉴
 			Common.clickByText("설정"); //설정 클릭
@@ -45,20 +45,20 @@ public class Func_04_InGame extends SetUp{
 	  }
 		
 		@Test
-		public void Home_004_InGame_BlockTitle() throws InterruptedException {
+		public void InGame_004_BlockTitle() throws InterruptedException {
 
 			Common.Assert_ExistByXpath("/html/body/div[1]/div[1]/div[2]/div[1]/div[1]/div[3]/div[2]/span/span"); //인게임 블록 제목 Xpath 확인
 	  }
 		
 		@Test
-		public void Home_005_InGame_BlockFollow() throws InterruptedException {
+		public void InGame_005_BlockFollow() throws InterruptedException {
 
 			Common.clickByText("블록 팔로우"); //블록 팔로우 버튼 클릭
 			Common.Assert_ExistByText(" 팔로우 중"); //팔로우 중 상태 확인
 	  }
 		
 		@Test
-		public void Home_006_InGame_BlockUnfollow() throws InterruptedException {
+		public void InGame_006_BlockUnfollow() throws InterruptedException {
 
 			Thread.sleep(1000);
 			Common.clickByText(" 팔로우 중"); //블록 팔로우 버튼 클릭
@@ -67,34 +67,34 @@ public class Func_04_InGame extends SetUp{
 	  }
 		
 		@Test
-		public void Home_007_InGame_ZoomOut() throws InterruptedException {
+		public void InGame_007_ZoomOut() throws InterruptedException {
 
 			Common.clickByXpath("/html/body/div[1]/div[1]/div[2]/div[1]/div[2]/div[2]/div[3]/img"); //축소 돋보기 버튼 클릭
 			Assert.assertEquals(driver.findElement(By.xpath("/html/body/div[1]/div[1]/div[2]/div[1]/div[2]/div[2]/div[1]")).getText(), "50%");
 	  }
 		
 		@Test
-		public void Home_008_InGame_ZoomIn() throws InterruptedException {
+		public void InGame_008_ZoomIn() throws InterruptedException {
 
 			Common.clickByXpath("/html/body/div[1]/div[1]/div[2]/div[1]/div[2]/div[2]/div[2]/img"); //확대 돋보기 버튼 클릭
 			Assert.assertEquals(driver.findElement(By.xpath("/html/body/div[1]/div[1]/div[2]/div[1]/div[2]/div[2]/div[1]")).getText(), "100%");
 	  }
 		
 		@Test
-		public void Home_009_InGame_User() throws InterruptedException {
+		public void InGame_009_User() throws InterruptedException {
 
 			System.out.println(driver.findElement(By.xpath("/html/body/div[1]/div[1]/div[2]/div[1]/div[2]/div[3]/div[2]")).getText()); //현재 인원수 프린트
 	  }
 		
 		@Test
-		public void Home_010_InGame_MenuBtn() throws InterruptedException {
+		public void InGame_010_MenuBtn() throws InterruptedException {
 
 			Common.clickByXpath("//html/body/div[1]/div[1]/div[2]/div[1]/div[2]/div[3]/div[3]/img"); //햄버거 메뉴 버튼 클릭
 			Common.Assert_ExistByXpath("/html/body/div[1]/div[1]/div[1]/div[2]/div/div/div[4]/div/div/div/div/div/div[2]/div"); //참가 중인 인원 확인
 	  }
 		
 //		@Test
-//		public void Home_011_InGame_1Chat1() throws InterruptedException {
+//		public void InGame_011_1Chat1() throws InterruptedException {
 //			// 1명 이상은 항상 블록에 있어야 하므로 고민 필요
 //			Actions actions = new Actions(driver);
 //			WebElement elementLocator = driver.findElement(By.xpath("/html/body/div[1]/div[1]/div[1]/div[2]/div/div/div[4]/div/div/div/div/div/div[2]/div"));
@@ -104,7 +104,7 @@ public class Func_04_InGame extends SetUp{
 //			Common.Assert_ExistByXpath("/html/body/div[1]/div[1]/div[1]/div[2]/div/div/div[4]/div/div/div/div/div/div[2]/div"); //참가 중인 인원 확인
 //	  }
 		@Test
-		public void Home_011_InGame_SearchNickname() throws InterruptedException {
+		public void InGame_011_SearchNickname() throws InterruptedException {
 
 			Common.waitForElementByXpath("/html/body/div[1]/div[1]/div[1]/div[2]/div/div/div[2]/input");
 			driver.findElement(By.xpath("/html/body/div[1]/div[1]/div[1]/div[2]/div/div/div[2]/input")).sendKeys("라일리"); //닉네임 검색
@@ -113,41 +113,41 @@ public class Func_04_InGame extends SetUp{
 	  }
 		
 		@Test
-		public void Home_012_InGame_CloseMenu() throws InterruptedException {
+		public void InGame_012_CloseMenu() throws InterruptedException {
 
 			Common.clickByXpath("/html/body/div[1]/div[1]/div[2]/div[1]/div[2]/div[3]/div[3]/img"); //햄버거 메뉴 닫기
 	  }
 		
 		@Test
-		public void Home_013_InGame_Open_Minimap() throws InterruptedException {
+		public void InGame_013_Open_Minimap() throws InterruptedException {
 
 			Common.clickByXpath("/html/body/div[1]/div[1]/div[1]/div[3]/div[3]/div[1]/img"); //미니맵 열기
 			Common.Assert_ExistByXpath("/html/body/div[1]/div[1]/div[1]/div[3]/div[3]/div[2]/img[1]"); // 미니맵 오픈 확인
 	  }
 		
 		@Test
-		public void Home_014_InGame_Close_Minimap() throws InterruptedException {
+		public void InGame_014_Close_Minimap() throws InterruptedException {
 
 			Common.clickByXpath("/html/body/div[1]/div[1]/div[1]/div[3]/div[3]/div[1]/img"); //미니맵 닫기
 			Common.Assert_NotExistByXpath("/html/body/div[1]/div[1]/div[1]/div[3]/div[3]/div[2]/img[2]"); // 미니맵 닫힘 확인
 	  }
 		
 		@Test
-		public void Home_015_InGame_Open_Notice() throws InterruptedException {
+		public void InGame_015_Open_Notice() throws InterruptedException {
 
 			Common.clickByXpath("/html/body/div[1]/div[1]/div[1]/div[3]/div[2]/div[1]/div/img"); //공지사항 열기
 			Common.Assert_ExistByXpath("/html/body/div[1]/div[1]/div[1]/div[3]/div[2]/div[2]/div"); // 공지사항 열림 확인
 	  }
 		
 		@Test
-		public void Home_016_InGame_Close_Notice() throws InterruptedException {
+		public void InGame_016_Close_Notice() throws InterruptedException {
 
 			Common.clickByXpath("/html/body/div[1]/div[1]/div[1]/div[3]/div[2]/div[1]/div/img"); //공지사항 닫기
 			Common.Assert_NoSuch_ByXpath("/html/body/div[1]/div[1]/div[1]/div[3]/div[2]/div[2]/div"); // 공지사항 닫힘 확인
 	  }
 		
 		@Test
-		public void Home_017_InGame_AllChatting() throws InterruptedException {
+		public void InGame_017_AllChatting() throws InterruptedException {
 
 			Common.waitForElementByClassName("chatting-input");
 			driver.findElement(By.className("chatting-input")).sendKeys("It's UI Test Automaiton Chatting with Quality Assurance."); //전체 채팅 입력
@@ -157,7 +157,7 @@ public class Func_04_InGame extends SetUp{
 	  }
 		
 		@Test
-		public void Home_018_InGame_OnOff_Chatting() throws InterruptedException {
+		public void InGame_018_OnOff_Chatting() throws InterruptedException {
 
 			Common.clickByXpath("/html/body/div[1]/div[1]/div[1]/div[3]/div[1]/div[2]/div[1]/div[3]/img"); //채팅 창 닫기
 			Common.Assert_ExistByXpath("/html/body/div[1]/div[1]/div[1]/div[3]/div[1]/img"); // 채팅 창 닫힘 확인
@@ -167,7 +167,7 @@ public class Func_04_InGame extends SetUp{
 	  }
 		
 		@Test
-		public void Home_019_InGame_FilterEffect() throws InterruptedException {
+		public void InGame_019_FilterEffect() throws InterruptedException {
 
 			Common.clickByXpath("/html/body/div[1]/div[1]/div[2]/div[2]/div[1]"); //필터 효과 모달 열기
 			Common.Assert_ExistByText("미디어 설정"); // 미디어 설정 모달 열림 확인		
@@ -175,7 +175,7 @@ public class Func_04_InGame extends SetUp{
 	  }
 		
 		@Test
-		public void Home_020_InGame_Emoji() throws InterruptedException {
+		public void InGame_020_Emoji() throws InterruptedException {
 
 			Common.clickByXpath("/html/body/div[1]/div[1]/div[2]/div[2]/div[2]/div[1]"); // 1번 이모티콘
 			Thread.sleep(1000);
@@ -188,14 +188,14 @@ public class Func_04_InGame extends SetUp{
 	  }
 		
 		@Test
-		public void Home_021_InGame_ShareScreen() throws InterruptedException {
+		public void InGame_021_ShareScreen() throws InterruptedException {
 
 			Common.clickByXpath("/html/body/div[1]/div[1]/div[2]/div[3]/div[2]/div[1]/div/img"); // 화면 공유 클릭
 			Thread.sleep(1000);
 	  }
 		
 		@Test
-		public void Home_022_InGame_OnOff_Mike() throws InterruptedException {
+		public void InGame_022_OnOff_Mike() throws InterruptedException {
 
 			Common.clickByXpath("/html/body/div[1]/div[1]/div[2]/div[3]/div[4]/div[2]/div/div"); //마이크 on
 			Thread.sleep(2000);
@@ -205,7 +205,7 @@ public class Func_04_InGame extends SetUp{
 	  }
 		
 		@Test
-		public void Home_023_InGame_OnOff_Camera() throws InterruptedException {
+		public void InGame_023_OnOff_Camera() throws InterruptedException {
 
 			Common.clickByText("카메라 off"); //카메라 on
 			Thread.sleep(2000);
---
+++ src/test/java/Func_05_Character_Customization.java
@@ -0,0 +1,132 @@
+package test.java;
+
+
+
+
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Wait;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import main.java.SetUp;
+
+
+public class Func_05_Character_Customization extends SetUp{
+	
+	
+		@Test
+		public void Character_001_MyMenu_Character() throws InterruptedException {
+			
+			//Common.facebookLogin(); //페이스북 로그인
+			
+			Common.clickByXpath("/html/body/div[1]/main/div/div[1]/div/div/div[1]/span[3]"); //빅배너에서 소개 블록 선택
+			Common.clickByXpath("/html/body/div[1]/main/div/div[1]/div/div/div[2]/div[4]/div/div/div[4]/span");//자세히 보기
+			Common.clickByXpath("/html/body/div[1]/div[1]/div[2]/div[1]/div[1]/div[2]/div/img"); //마이메뉴
+			
+			//Common.clickByXpath("/html/body/div[1]/header/div/div[2]/div[2]/div"); //마이메뉴
+			Common.clickByText("캐릭터 꾸미기"); //"캐릭터 꾸미기" 클릭
+			
+	  }
+
+		@Test
+		public void Character_002_Skin() throws InterruptedException {
+			Common.Assert_ExistByText("피부"); //캐릭터꾸미기 모달 진입 시 피부 디폴트 노출 확인
+			Thread.sleep(2000);
+			Common.clickByXpath("/html/body/div[3]/div/div/div[2]/div[2]/div[2]/ul/li[3]"); //스킨 선택
+		}
+		@Test
+		public void Character_003_Hair() throws InterruptedException {
+			
+			Common.clickByText("헤어");
+			Thread.sleep(2000);
+			Common.clickByXpath("/html/body/div[3]/div/div/div[2]/div[2]/div[2]/ul/li[12]"); //헤어 선택
+			Common.clickByXpath("/html/body/div[3]/div/div/div[2]/div[2]/div[3]/ul/li[4]/div"); //헤어 색상 선택
+	  }
+		
+		@Test
+		public void Character_004_Top() throws InterruptedException {
+			
+			Thread.sleep(2000);
+			Common.clickByXpath("/html/body/div[3]/div/div/div[2]/div[2]/div[1]/ul/li[3]"); //상의 탭 선택
+			Thread.sleep(2000);
+			Common.clickByXpath("/html/body/div[3]/div/div/div[2]/div[2]/div[2]/ul/li[6]"); //상의 선택
+			Common.clickByXpath("/html/body/div[3]/div/div/div[2]/div[2]/div[3]/ul/li[3]/div"); //상의 색상 선택
+	  }
+		
+		@Test
+		public void Character_005_Bottom() throws InterruptedException {
+			Common.clickByXpath_Hidden("/html/body/div[3]/div/div/div[2]/div[2]/div[1]/div[2]/button");
+			Common.clickByText("하의");
+			Thread.sleep(2000);
+			Common.clickByXpath("/html/body/div[3]/div/div/div[2]/div[2]/div[2]/ul/li[9]"); //하의 선택
+			Common.clickByXpath("/html/body/div[3]/div/div/div[2]/div[2]/div[3]/ul/li[3]/div"); //하의 색상 선택
+	  }
+		
+		@Test
+		public void Character_006_Bag() throws InterruptedException {
+			
+			Common.clickByXpath_Hidden("/html/body/div[3]/div/div/div[2]/div[2]/div[1]/div[4]/button");
+			Common.clickByXpath_Hidden("/html/body/div[3]/div/div/div[2]/div[2]/div[1]/div[4]/button");
+
+			Thread.sleep(2000);
+			Common.clickByText("가방");
+			Thread.sleep(2000);
+			Common.clickByXpath("/html/body/div[3]/div/div/div[2]/div[2]/div[2]/ul/li[6]"); //가방 선택
+	  }
+		
+		@Test
+		public void Character_007_Cap() throws InterruptedException {
+			
+			Common.clickByText("모자");
+			Thread.sleep(2000);
+			Common.clickByXpath("/html/body/div[3]/div/div/div[2]/div[2]/div[2]/ul/li[2]"); //모자 선택
+	  }
+		
+		@Test
+		public void Character_008_Shose() throws InterruptedException {
+			Common.clickByXpath_Hidden("/html/body/div[3]/div/div/div[2]/div[2]/div[1]/div[4]/button");
+			Common.clickByXpath_Hidden("/html/body/div[3]/div/div/div[2]/div[2]/div[1]/div[4]/button");
+			Common.clickByXpath_Hidden("/html/body/div[3]/div/div/div[2]/div[2]/div[1]/div[4]/button");
+			
+			Common.clickByText("신발");
+			Thread.sleep(2000);
+			Common.clickByXpath("/html/body/div[3]/div/div/div[2]/div[2]/div[2]/ul/li[6]"); //신발 선택
+	  }
+		
+		@Test
+		public void Character_009_Socks() throws InterruptedException {
+			
+			Common.clickByText("양말");
+			Thread.sleep(2000);
+			Common.clickByXpath("/html/body/div[3]/div/div/div[2]/div[2]/div[2]/ul/li[7]"); //양말 선택
+	  }
+		
+		@Test
+		public void Character_010_Accessories() throws InterruptedException {
+			
+			Common.clickByText("악세사리");
+			Thread.sleep(2000);
+			Common.clickByXpath("/html/body/div[3]/div/div/div[2]/div[2]/div[2]/ul/li[1]"); //악세사리 1 선택
+			Thread.sleep(2000);
+			Common.clickByXpath("/html/body/div[3]/div/div/div[2]/div[2]/div[2]/ul/li[7]"); //악세사리 7 선택
+			Thread.sleep(2000);
+			Common.clickByXpath("/html/body/div[3]/div/div/div[2]/div[2]/div[2]/ul/li[8]"); //악세사리 8 선택
+	  }
+		
+		@Test
+		public void Character_011_Save() throws InterruptedException {
+			
+			Common.clickByText("저장");
+			Common.clickByText("확인");
+			Thread.sleep(2000);
+	  }
+		
+}
