038413e8eaa345d8944f84cabaea48b9905c52ad
riley <riley@DESKTOP-IIOPE3V>
2022-07-19 09:43:41 +0900

    회원탈퇴 버튼의 xpath 변경으로 수정

--- src/test/java/DeleteAccount.java
+++ src/test/java/DeleteAccount.java
@@ -44,8 +44,8 @@ public class DeleteAccount extends SetUp{
 	    Common.clickByXpath("/html/body/div[1]/header/div/div[2]/div[2]/div"); //마이메뉴
 	    Common.clickByText("설정");
 	    Common.clickByText("정말 탈퇴하시겠습니까?");
-	    Common.clickByXpath("/html/body/div[16]/div/div/div/div[4]/div[2]/button");//회원탈퇴 버튼
-	    
+	    Common.clickByXpath("/html/body/div[11]/div/div/div/div[4]/div[2]/button");//회원탈퇴 버튼
+	                        
 	    Common.clickByText("확인");
 	    
 //	    Common.clickByXpath("/html/body/div[1]/header/div/div[2]/div[2]/ul/li[5]"); //설정
