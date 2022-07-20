8cbec0d197a95f1040bec4d31bd7d519c4eb986a
riley <riley@DESKTOP-IIOPE3V>
2022-07-20 14:58:52 +0900

    블록만들기 xpath 수정

--- src/test/java/Func_07_MakeBlock.java
+++ src/test/java/Func_07_MakeBlock.java
@@ -44,18 +44,18 @@ public class Func_07_MakeBlock extends SetUp{
 			
 		    Common.Assert_ExistByText("#대규모컨퍼런스홀"); //템플릿 제목
 		    Thread.sleep(1000);
-		    Common.Assert_ExistByXpath("/html/body/div[10]/div/div/div[2]/div[1]/div[1]/img"); //썸네일
+		    Common.Assert_ExistByXpath("/html/body/div[9]/div/div/div[2]/div[1]/div[1]/img"); //썸네일
 
 		    Thread.sleep(1000);
-		    Assert.assertEquals(driver.findElement(By.xpath("/html/body/div[10]/div/div/div[2]/div[1]/p[2]")).getText(), "깔끔한 화이트톤의 약 300인 좌석 컨퍼런스 홀입니다."); //설명
+		    Assert.assertEquals(driver.findElement(By.xpath("/html/body/div[9]/div/div/div[2]/div[1]/p[2]")).getText(), "깔끔한 화이트톤의 약 300인 좌석 컨퍼런스 홀입니다."); //설명
 		    Thread.sleep(1000);
-		    Common.Assert_ExistByXpath("/html/body/div[10]/div/div/div[2]/div[2]/div[2]/div/input"); //블록 이름 입력란
+		    Common.Assert_ExistByXpath("/html/body/div[9]/div/div/div[2]/div[2]/div[2]/div/input"); //블록 이름 입력란
 		}
 		
 		@Test
 		public void MakeBlock_004_Preset_CheckBtn() throws InterruptedException {
 			
-		    Assert.assertFalse(driver.findElement(By.xpath("/html/body/div[10]/div/div/div[2]/div[2]/button")).isEnabled()); //버튼 비활성화 체크
+		    Assert.assertFalse(driver.findElement(By.xpath("/html/body/div[9]/div/div/div[2]/div[2]/button")).isEnabled()); //버튼 비활성화 체크
 		}
 		
 		@Test
@@ -67,15 +67,15 @@ public class Func_07_MakeBlock extends SetUp{
 //			String formattedDate = sdf.format(date);
 //			driver.findElement(By.xpath("/html/body/div[7]/div/div/div[3]/div/div[1]/div[1]/div[2]/div[2]/input")).sendKeys("라일리"+ formattedDate); //닉네임 입력
 //			
-			Common.InputTextXpath("/html/body/div[10]/div/div/div[2]/div[2]/div[2]/div/input", "라일리첫블록"); //블록 제목 라일리 입력
-			Common.Enter("/html/body/div[10]/div/div/div[2]/div[2]/div[2]/div/input"); //엔터
+			Common.InputTextXpath("/html/body/div[9]/div/div/div[2]/div[2]/div[2]/div/input", "라일리첫블록"); //블록 제목 라일리 입력
+			Common.Enter("/html/body/div[9]/div/div/div[2]/div[2]/div[2]/div/input"); //엔터
 			
 		}
 		
 		@Test
 		public void MakeBlock_006_Preset_Make() throws InterruptedException {
 			
-			Common.clickByXpath("/html/body/div[10]/div/div/div[2]/div[2]/button"); //블록 만들기 버튼 클릭
+			Common.clickByXpath("/html/body/div[9]/div/div/div[2]/div[2]/button"); //블록 만들기 버튼 클릭
 			Thread.sleep(5000);
 			Common.Assert_ExistByXpath("/html/body/div[1]/div[1]/div[2]/div[1]/div[1]/div[3]/div[2]/span/span"); //인게임 블록 제목 Xpath 확인
 			Thread.sleep(1000);
